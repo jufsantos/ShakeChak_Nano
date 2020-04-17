50acfe405116da49f1847a1264049710581b930c
julia <julia.fsantos2@gmail.com>
2020-04-17 19:09:19 -0300

    17.04 deliver

--- ShakeChak/ShakeChak/ViewController.swift
+++ ShakeChak/ShakeChak/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var image1: UIImageView!
+    
+    let motion = CMMotionManager()
+    
+   public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        startAccelerometers()
+        
     }
+    func startAccelerometers() {
+        // Make sure the accelerometer hardware is available.
+        
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+            self.motion.startAccelerometerUpdates()
 
-
+            
+            // Configure a timer to fetch the data.
+            let timer = Timer(fire: Date(), interval: (1.0/60.0),
+                              repeats: true, block: { (timer) in
+                                // Get the accelerometer data.
+                                if let data = self.motion.accelerometerData {
+                                    
+                                    let x = data.acceleration.x
+                                    let y = data.acceleration.y
+                                    let z = data.acceleration.z
+                                    
+                                    print(x, y, z)
+                                    
+                                    if abs(x) > 1 || abs(y) > 1 || abs(z) > 1 {
+                                        DispatchQueue.main.async {
+                                            self.image1.image = UIImage(named: "chocalho")
+                                        }
+                                    } else{
+                                        DispatchQueue.main.async {
+                                            self.image1.image = UIImage(named: "chocalho2")
+                                        }
+                                    }
+                                }
+            })
+            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+        }
+    }
 }
 
