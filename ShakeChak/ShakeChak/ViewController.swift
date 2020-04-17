4f12251f2fe8b416a8b23f823c2fe3c83d3e9b0c
julia <julia.fsantos2@gmail.com>
2020-04-17 17:07:25 -0300

    first commit

---
+++ ShakeChak/ShakeChak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShakeChak
+//
+//  Created by Julia Santos on 17/04/20.
+//  Copyright © 2020 Julia Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
